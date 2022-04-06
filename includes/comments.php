a528d70c2ab82c82bc527b8f6c76177b9e8a9c1d
黑白 <2285697543@qq.com>
2022-04-06 21:51:03 +0800

    清空

--- includes/comments.php
+++
@@ -1,5 +0,0 @@
- <?php if($this->options->diyavatar && $this->options->diyavatar=1): ?>
-   <?php $this->need('includes/comments3.php'); ?>
-<?php else: ?>
-    <?php $this->need('includes/comments2.php'); ?>
-<?php endif; ?>
